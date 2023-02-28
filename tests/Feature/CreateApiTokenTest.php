bedba26bfba0d71d98547977fa2dfccdb0008937
Dayan Betancourt <dkbetancourt@gmail.com>
2023-02-28 10:25:26 -0400

    test :white_check_mark: Add Tests

---
+++ tests/Feature/CreateApiTokenTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\{User, Occupation};
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Jetstream\Features;
+use Tests\TestCase;
+
+class CreateApiTokenTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_api_tokens_can_be_created(): void
+    {
+        if (! Features::hasApiFeatures()) {
+            $this->markTestSkipped('API support is not enabled.');
+
+            return;
+        }
+
+        $occupation = Occupation::factory()->create(['name' => 'Programación']);
+        $this->actingAs($user = User::factory()->withPersonalTeam()->create(['occupation_id' => $occupation->id]));
+
+        $response = $this->post('/user/api-tokens', [
+            'name' => 'Test Token',
+            'permissions' => [
+                'read',
+                'update',
+            ],
+        ]);
+
+        $this->assertCount(1, $user->fresh()->tokens);
+        $this->assertEquals('Test Token', $user->fresh()->tokens->first()->name);
+        $this->assertTrue($user->fresh()->tokens->first()->can('read'));
+        $this->assertFalse($user->fresh()->tokens->first()->can('delete'));
+    }
+}
